f1ca39bcf783b084db4f8b7a350e453c819dab88
Gael Lucero <gael@udel.edu>
2025-03-09 15:45:54 -0400

    Completed CheckAnswer form feature. Updated nested.ts and package-lock.json

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,32 @@
 import React, { useState } from "react";
+import { FormControl, FormGroup, FormLabel } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    // This is the state (model)
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
+    // This is the Conrtol, where we update the userAnswer with what they input
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserAnswer(event.target.value);
+    }
+
+    // this is the View, where we display the textbox (FormGroup) and update the userAnswer State when user types.
     return (
         <div>
-            <h3>Check Answer</h3>
+            {/* <h3>Check Answer</h3> */}
+            <FormGroup controlId="formUserAnswer">
+                <FormLabel>Input Answer Below:</FormLabel>
+                <FormControl
+                    value={userAnswer}
+                    onChange={updateAnswer}
+                ></FormControl>
+            </FormGroup>
+            {/* The checkmark will display when the userAnswer State matchesthe expectedAnswer, else X */}
+            <div>{userAnswer === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
